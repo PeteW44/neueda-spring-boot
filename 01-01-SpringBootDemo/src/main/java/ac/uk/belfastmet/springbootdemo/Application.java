7e90af49107a52fa9891d366087e90cf19f2cbac
WIG10002179 <peterwightman44@btinternet.com>
2017-11-27 15:14:46 +0000

    Update

--- 01-01-SpringBootDemo/src/main/java/ac/uk/belfastmet/springbootdemo/Application.java
+++ 01-01-SpringBootDemo/src/main/java/ac/uk/belfastmet/springbootdemo/Application.java
@@ -1,12 +1,31 @@
-package ac.uk.belfastmet.springbootdemo;
+/*
+ * Class:           Application.java
+ * Project:         Spring Boot Demo
+ * Package:         ac.uk.belfastmet.springbootdemo
+ * Version:         1.0
+ * Created:         27/11/2017
+ * Updated:         27/11/2017 15.30
+ * Author:          Peter Wightman
+ * Description:     This is the Application Class
+ */
 
+package ac.uk.belfastmet.springbootdemo;
+//Import Packages
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class Application {
-
-	public static void main(String[] args) {
+public class Application
+{
+	private static final Logger logger = LoggerFactory.getLogger(Application.class);
+	
+	public static void main(String[] args)
+	{
 		SpringApplication.run(Application.class, args);
+		
+		logger.info("Hellow Spring Boot!");
+		logger.info("Hellow Again");
 	}
 }
