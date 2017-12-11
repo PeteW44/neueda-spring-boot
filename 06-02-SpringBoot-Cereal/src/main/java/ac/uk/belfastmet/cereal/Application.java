4c944ae9d35462eb372c79d5a1ee8d91cd32b4e5
WIG10002179 <peterwightman44@btinternet.com>
2017-12-11 16:27:56 +0000

    End Of School Day

---
+++ 06-02-SpringBoot-Cereal/src/main/java/ac/uk/belfastmet/cereal/Application.java
@@ -0,0 +1,12 @@
+package ac.uk.belfastmet.cereal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Application.class, args);
+	}
+}
